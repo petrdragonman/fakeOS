06280001c22b8720ea2123c41f2a77e1c59e8d00
petrdragonman <petrdragonman@me.com>
2024-12-25 09:24:58 +1100

    implements current time on the toolbar

---
+++ scripts/Maths/getCurrentTime.js
@@ -0,0 +1,19 @@
+export const getCurrentTime = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            //// dummy function creating the 1 sec wait
+            resolve(1);
+        }, 1000);
+    });
+}
+
+export const updateClock = async (elId) => {
+    await getCurrentTime();
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+    const timeString = `${hours.toString().padStart(2,"0")}:${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`;
+    // use the elId and update .innerText
+    document.getElementById(elId).innerText = timeString; 
+};
